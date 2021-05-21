0f84a5f12db9dcce580434253c05b6a6cb21eaf6
Ashley Fong <>
2021-05-21 19:10:00 +0000

    javadocs and comments

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -2,22 +2,44 @@ package recursionAssignment;
 
 public class Recursion{
 
-  //problem set 1
-  public static int count7 (int n){ 
+  /**
+   * Problem Set 1
+   *
+   * Given a non-negative int n, return the count of the occurrences of 7 as a digit
+   *
+   * @param n The value that is being checked 
+   * @return the number of times 7 occurs 
+   * @author A. Fong
+   * 
+   */
+  public static int count7 (int n) { 
+    
+    //if n < 7, return 0
     if (n < 7) {
       return 0; 
     }
 
-    else if (n % 10 == 7){ 
-      return 1 + count7(n / 10); 
+    //if the right most number is 7, return 1 and the number of times 7 occurs in the rest of the input 
+    else if (n % 10 == 7 { 
+      return 1 + count7 (n / 10); 
     }
 
+    //if the right digit is not 7, return the number of 7s in the rest of the input 
     else { 
-      return count7(n / 10); 
+      return count7 (n / 10); 
     } 
   }
 
-  //problem set 2
+  /**
+   * Problem Set 2
+   *
+   * Given a string, compute a new string where all appearances of "pi" have been replaced by "3.14"
+   *
+   * @param str The string that is being changed  
+   * @return str but every occurance of pi is replaced with 3.14 
+   * @author A. Fong
+   * 
+   */
   public static String changePi(String str){ 
     
     if (str.length() < 2){ 
@@ -33,7 +55,16 @@ public class Recursion{
     }
   }
 
-  //Problem Set 3
+  /**
+   * Problem Set 3
+   *
+   * Given a string, return a "cleaned" string where adjacent chars that are the same have been reduced to a single char 
+   *
+   * @param str The string that is being reduced 
+   * @return str but every occurance of adjacent chars that are the same is reduced to one 
+   * @author A. Fong
+   * 
+   */
   public static String stringClean (String str) { 
     
     if (str.length() <= 1){
